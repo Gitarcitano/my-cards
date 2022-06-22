6bdcd9f9d66d1b0205a20bfd06f6f9863e50727c
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2022-06-22 13:43:56 -0300

    Revert "Started working on a new light card. I am tired of playing with custom-card and jinja templating.. It will be customizable to some extend (probably better than the slider since I have learned a little even though it doesn't look like it on my code.). And I have not forgotten about updates to the slider. I will most likely be looking at a couple of the issues and update it soon. (hopefully)."
    This reverts commit 0eb06c9ee1b2505e587c35fb9458b37ea020ca0f.

--- src/my-dashboard.ts
+++ src/my-dashboard.ts
@@ -145,14 +145,13 @@ export class MyDashboard extends LitElement {
             card: {
                 height: '100%',
                 width: '100%',
-                background: 'rgba(255,0,0,0.15)',
+                background: 'transparent',
                 borderRadius: '0px',
                 boxShadow: 'none',
                 color: 'var(--primary-text-color)',
                 display: 'block',
                 transition: 'all 0.3s ease-out 0s',
-                position: 'relative',
-                // border: '1px solid red'
+                position: 'relative'
             }
         }
     }
